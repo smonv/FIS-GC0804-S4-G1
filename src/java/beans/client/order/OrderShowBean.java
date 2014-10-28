4b3f98644a8b8ffe135bbcd03c0ce81bcef884d0
thanhvt <thanhvt1609@gmail.com>
2014-10-28 17:04:06 +0700

    show order details

--- src/java/beans/client/order/OrderShowBean.java
+++ src/java/beans/client/order/OrderShowBean.java
@@ -7,10 +7,14 @@ package beans.client.order;
 
 import entities.OrderProductDetails;
 import entities.Orders;
+import entities.Products;
+import helpers.ApplicationHelper;
+import java.io.IOException;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import models.OrderModel;
 import models.ProductModel;
 
@@ -30,10 +34,25 @@ public class OrderShowBean {
 
     private List<Orders> orders;
 
+    private int oid;
+
+    private Orders order;
+
+    private Products product;
+
     /**
      * Creates a new instance of OrderShowBean
      */
     public OrderShowBean() {
+    }
+
+    public void init() throws IOException {
+        if (!FacesContext.getCurrentInstance().isPostback()) {
+            if (oid == 0 && !orderModel.orderExists(oid)) {
+                ApplicationHelper.redirect("/404.xhtml", false);
+            }
+            getOrder();
+        }
     }
 
     public int getTotalOrderProduct(Orders order) {
@@ -57,8 +76,40 @@ public class OrderShowBean {
         return orders;
     }
 
+    public Products getCurrentProduct(int pid) {
+        product = productModel.getById(pid);
+        return product;
+    }
+
     public void setOrders(List<Orders> orders) {
         this.orders = orders;
+    }
+
+    public int getOid() {
+        return oid;
+    }
+
+    public void setOid(int oid) {
+        this.oid = oid;
+    }
+
+    public Orders getOrder() {
+        if (order == null) {
+            order = orderModel.getById(oid);
+        }
+        return order;
+    }
+
+    public void setOrder(Orders order) {
+        this.order = order;
+    }
+
+    public Products getProduct() {
+        return product;
+    }
+
+    public void setProduct(Products product) {
+        this.product = product;
     }
 
 }
