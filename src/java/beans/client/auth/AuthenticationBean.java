929a616a7a4fca1772775b7be1ac8a4c62bb5224
SolomonT <thanhvt1609@gmail.com>
2014-11-02 01:06:17 +0700

    feature authorization

---
+++ src/java/beans/client/auth/AuthenticationBean.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans.client.auth;
+
+import entities.Clients;
+import helpers.ApplicationHelper;
+import helpers.SessionHelper;
+import java.util.Map;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import models.ClientModel;
+
+/**
+ *
+ * @author Cu Beo
+ */
+@ManagedBean
+@RequestScoped
+public class AuthenticationBean {
+
+    @EJB
+    private ClientModel clientModel;
+
+    String username;
+    String password;
+    boolean status;
+    private String message;
+
+    public AuthenticationBean() {
+    }
+
+    public void authenticate() {
+        Map<String, Object> session = SessionHelper.getSessionMap();
+        if (session.get("msg") != null) {
+            session.remove("msg");
+        }
+        Clients client = clientModel.authenticate(username, password);
+        client.setPasswordDigest("");
+        if (client != null) {
+            ApplicationHelper.addMessage("Logged!");
+            SessionHelper.getSessionMap().put("client", client);
+            ApplicationHelper.redirect("/index.xhtml", true);
+        } else {
+            ApplicationHelper.addMessage("Wrong Username / Password");
+            ApplicationHelper.redirect("/login.xhtml", true);
+        }
+    }
+
+    public void logout() {
+        Map<String, Object> session = SessionHelper.getSessionMap();
+        if (session.get("client") != null) {
+            session.remove("client");
+            ApplicationHelper.redirect("/index.xhtml", true);
+        } else {
+            ApplicationHelper.redirect("/403.xhtml", true);
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        Map<String, Object> session = SessionHelper.getSessionMap();
+        message = session.get("msg") != null ? session.get("msg").toString() : null;
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
---
+++ src/java/filters/FilterClient.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author SolomonT
+ */
+@WebFilter("/client/*")
+public class FilterClient implements Filter {
+    
+    private FilterConfig config;
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.config = filterConfig;
+    }
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession();
+        
+        if (session.getAttribute("client") == null) {
+            session.setAttribute("msg", "Please log in to access that page!");
+            res.sendRedirect(req.getContextPath() + "/login.xhtml");
+        } else if (session.getAttribute("admin") != null) {
+            res.sendRedirect(req.getContextPath() + "/403.xhtml");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+    
+    @Override
+    public void destroy() {
+        this.config = null;
+    }
+    
+}
