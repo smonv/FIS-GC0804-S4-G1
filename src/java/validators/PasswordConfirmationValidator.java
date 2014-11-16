33dc67487c7add24ec6451f6836d81344d6a1c4e
SolomonT <thanhvt1609@gmail.com>
2014-11-16 22:07:06 +0700

    fix validate password confirmation null

--- src/java/validators/PasswordConfirmationValidator.java
+++ src/java/validators/PasswordConfirmationValidator.java
@@ -19,18 +19,20 @@ import javax.faces.validator.ValidatorException;
  */
 @FacesValidator("passwordConfirmationMatch")
 public class PasswordConfirmationValidator implements Validator {
-    
+
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         String password_confirmation = value.toString();
         UIInput uiInputPassword = (UIInput) context.getViewRoot().findComponent("registerForm:password");
-        String password = uiInputPassword.getValue().toString();
+        if (uiInputPassword.getValue() != null) {
+            String password = uiInputPassword.getValue().toString();
 
-        if (!password.equals(password_confirmation)) {
-            uiInputPassword.setValid(false);
-            FacesMessage msg = new FacesMessage("Password must match password confirm!");
-            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-            throw new ValidatorException(msg);
+            if (!password.equals(password_confirmation)) {
+                uiInputPassword.setValid(false);
+                FacesMessage msg = new FacesMessage("Password must match password confirm!");
+                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+                throw new ValidatorException(msg);
+            }
         }
     }
 
